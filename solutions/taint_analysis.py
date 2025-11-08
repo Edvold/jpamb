085a6c01d4cb13142ec0303f8c4b999f15c34f33
BjarkeBak <bjarkejensen@live.dk>
2025-11-08 15:22:48 +0100

    bugfixes + array support

--- solutions/taint_analysis.py
+++ solutions/taint_analysis.py
@@ -36,14 +36,16 @@ def find_ident_in_children(ast):
 def flows(ast, unsafe_variables, implicit_variables=set()):
     res = set()
     for node in ast:
+        #logger.debug(node['type'])
+        #logger.debug(node['text'])
         match node['type']:
-            case 'assert_statement': # If we have an assert statement, we check if any unsafe variables are used. 
+            case 'assert_statement' | 'assert': # If we have an assert statement, we check if any unsafe variables are used. 
                 idents = find_ident_in_children(node['children'])
                 total_idents = set.union(idents, implicit_variables)
                 for var in unsafe_variables:
                     if var in total_idents:
                         res.add((var, 'assert'))
-            case 'return_statement':
+            case 'return_statement' | 'return':
                 idents = find_ident_in_children(node['children'])
                 total_idents = set.union(idents, implicit_variables)
                 for var in unsafe_variables:
@@ -53,12 +55,48 @@ def flows(ast, unsafe_variables, implicit_variables=set()):
                 if_childs = node['children']
                 if len(if_childs) > 2: # not an empty if
                     guard_expression = if_childs[1] #skipping the first child which is just an "if" node
-                    new_implicit_vars = set.union(find_ident_in_children(guard_expression['children']), implicit_variables)
-                    res = set.union(res, flows(if_childs[2]['children'], unsafe_variables, new_implicit_vars)) 
+                    new_implicit_vars = set.union(find_ident_in_children(guard_expression['children']), implicit_variables) #fix else if
+                    res = set.union(res, flows(if_childs[2]['children'], unsafe_variables, new_implicit_vars)) # not only if_childs[2]
+                    if len(if_childs) >= 5: # with else (how about more if elses? maybe goes back to here)
+                        res = set.union(res, flows(if_childs[4]['children'], unsafe_variables, new_implicit_vars))
                     # children will always exists
-
+            case 'while_statement':
+                while_childs = node['children']
+                if len(while_childs) > 2: # not an empty while loop
+                    guard_expression = while_childs[1] # skipping the "while" node
+                    new_implicit_vars = set.union(find_ident_in_children(guard_expression['children']), implicit_variables)
+                    res = set.union(res, flows(while_childs[2]['children'], unsafe_variables, new_implicit_vars))
+                #logger.debug(node['text'])
+            case 'for_statement':
+                for_childs = node['children']
+                if len(for_childs) > 4: # Thank you for being a normal human being (for loop with initialization, termination, increment and body)
+                    # for_childs[0] is just a node with text "for"
+                    init_flows = flows(for_childs[1]['children'], unsafe_variables, implicit_variables) # checking for flows in the initialization expression
+                    body_implicit_variables = set.union(find_ident_in_children(for_childs[2]['children']), implicit_variables) # checking for new implicit flow variables in termination expression
+                    update_expr_flows = flows(for_childs[3]['children'], unsafe_variables, body_implicit_variables)
+                    body_flows = flows(for_childs[4]['children'], unsafe_variables, body_implicit_variables)
+                    res = set.union(res, init_flows, update_expr_flows, body_flows)
+                elif len(for_childs) <= 4: # Who the fuck does this?
+                    exit(f"For loop with {len(for_childs)} arguments")
+            case 'expression_statement':
+                #logger.debug(node)
+                res = set.union(res, flows(node['children'], unsafe_variables, implicit_variables))
+            #case 'else':
+                #logger.debug(node)
+            #case 'block':
+                #logger.debug(node)
+            case 'assignment_expression':
+                #logger.debug(node)
+                ident_to = node['children'][0]['text'] # the identifier of the variable being assigned to
+                idents_from = find_ident_in_children(node['children'][1:]) # the identifier of variables using in expression
+                total_idents = set.union(idents_from, implicit_variables)
+                for var in unsafe_variables:
+                    if var in total_idents:
+                        res.add((var, ident_to))
             case _:
                 continue
+            # local variable declaration
+            # variable declarator
     return res
 
 # method to handle flow through method calls
@@ -160,14 +198,17 @@ else:
     body = None
     for node in captures["method"]:
         body = node.child_by_field_name("body")
-        #method_body = body.text.decode()
-        #logger.debug(method_body)
+        method_body = body.text.decode()
+        logger.debug(param_names)
+        logger.debug(method_body)
     
     # if not body == None:
     #     for stmt in body.children:
     #         print(stmt.type, stmt.text.decode())
 
     ast_body = to_ast(body)
-    logger.debug(ast_body['children'])
+    #logger.debug(ast_body['children'])
+    #for node in ast_body['children']:
+    #   logger.debug(node)
     flow_dict = flows(ast_body['children'], param_names)
     logger.debug(flow_dict)
