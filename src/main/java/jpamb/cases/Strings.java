8ed9de1f04bf5762545e9179a5abc3f9f149a487
Edvold <mathias@edvold.dk>
2025-11-10 13:57:57 +0100

    fix test cases

--- src/main/java/jpamb/cases/Strings.java
+++ src/main/java/jpamb/cases/Strings.java
@@ -2,6 +2,8 @@ package jpamb.cases;
 
 public class Strings {
 
+    @Case("(\"Hello World!\") -> ok")
+    @Case("(\"Hello World\") -> assertion error")
     public static void assertEqualManually(String s) {
         String t = "Hello World!";
 
@@ -12,11 +14,15 @@ public class Strings {
         }
     }
 
+    @Case("(\"Hello World!\") -> ok")
+    @Case("(\"Hello World\") -> assertion error")
     public static void assertEqualDirectly(String s) {
         String t = "Hello World!";
         assert t.equals(s);
     }
 
+    @Case("(\"World!\") -> ok")
+    @Case("(\"World\") -> assertion error")
     public static void assertConcatVars(String s) {
         String t = "Hello ";
         String u = t + s;
@@ -24,6 +30,8 @@ public class Strings {
         assert u.equals("Hello World!");
     }
 
+    @Case("(\"World!\") -> ok")
+    @Case("(\"World\") -> assertion error")
     public static void assertConcatConstants(String s) {
         String j = "Hello " + s;
         assert j.equals("Hello World!");
@@ -33,24 +41,39 @@ public class Strings {
         return s + "A";
     }
 
+    @Case("(\"Hello World!\") -> ok")
+    @Case("(\"Hello World\") -> assertion error")
     public static void assertReturn(String s) {
         assert concatA(s).equals("Hello World!A");
     }
 
+    @Case("(6) -> ok")
+    @Case("(7) -> assertion error")
+    @Case("(-1) -> out of bounds")
+    @Case("(13) -> out of bounds")
     public static void assertSubstring1(int lower) {
         String s = "Hello World!";
         assert s.substring(lower).equals("World!");
     }
 
+    @Case("(0, 5) -> ok")
+    @Case("(0, 6) -> assertion error")
+    @Case("(0, 13) -> out of bounds")
+    @Case("(-1, 3) -> out of bounds")
+    @Case("(5, 4) -> out of bounds")
     public static void assertSubstring2(int lower, int higher) {
         String s = "Hello World!";
         assert s.substring(lower, higher).equals("Hello");
     }
 
+    @Case("(\"Hello World!\", \"Hello\", 0) -> ok")
+    @Case("(\"Hello World!\", \"Hey\", -1) -> ok")
     public static void assertIndexOfString(String source, String target, int result) {
         assert source.indexOf(target) == result;
     }
 
+    @Case("(\"Hello World!\", 'l', 2) -> ok")
+    @Case("(\"Hello World!\", 'y', -1) -> ok")
     public static void assertIndexOfChar(String source, char target, int result) {
         assert source.indexOf(target) == result;
     }
