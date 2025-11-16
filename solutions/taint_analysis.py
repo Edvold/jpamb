2992db4e033b696fd724819feadd3ea6c452e72d
BjarkeBak <bjarkejensen@live.dk>
2025-11-16 14:11:10 +0100

    fail build

--- solutions/taint_analysis.py
+++ solutions/taint_analysis.py
@@ -22,10 +22,16 @@ def to_ast(node):
 
 def find_ident_in_children(ast): #add case for method_invocation to model sanitization
     idents = set()
+    should_skip = False
     for node in ast:
+        if should_skip:
+            should_skip = False
+            continue
         match node['type']:
             case 'identifier':
                 idents.add(node['text'])
+            case '.': #skipping method identifier from objects ex. equals in s.equals("...")
+                should_skip = True
             case _ if len(node['children']) > 0:
                 sub_idents = find_ident_in_children(node['children'])
                 idents = set.union(idents, sub_idents)
@@ -33,7 +39,7 @@ def find_ident_in_children(ast): #add case for method_invocation to model saniti
                 continue
     return idents
 
-def flows(ast, unsafe_variables, seen_methods, implicit_variables=set()):
+def flows(ast, seen_methods, implicit_variables=set()):
     res = set()
     for node in ast:
         #logger.debug(node['type'])
@@ -42,45 +48,45 @@ def flows(ast, unsafe_variables, seen_methods, implicit_variables=set()):
             case 'assert_statement' | 'assert': # If we have an assert statement, we check if any unsafe variables are used. 
                 idents = find_ident_in_children(node['children'])
                 total_idents = set.union(idents, implicit_variables)
-                for var in unsafe_variables:
-                    if var in total_idents:
-                        res.add((var, 'assert'))
+                #for var in unsafe_variables:
+                for var in total_idents:
+                    res.add((var, 'assert'))
             case 'return_statement' | 'return':
                 idents = find_ident_in_children(node['children'])
                 total_idents = set.union(idents, implicit_variables)
-                for var in unsafe_variables:
-                    if var in total_idents:
-                        res.add((var, 'return'))
+                #for var in unsafe_variables:
+                for var in total_idents:
+                    res.add((var, 'return'))
             case 'if_statement': #how to handle else statements?
                 if_childs = node['children']
                 if len(if_childs) > 2: # not an empty if
                     guard_expression = if_childs[1] #skipping the first child which is just an "if" node
                     new_implicit_vars = set.union(find_ident_in_children(guard_expression['children']), implicit_variables) #fix else if
-                    res = set.union(res, flows(if_childs[2]['children'], unsafe_variables, seen_methods, new_implicit_vars)) # not only if_childs[2]
+                    res = set.union(res, flows(if_childs[2]['children'], seen_methods, new_implicit_vars)) # not only if_childs[2]
                     if len(if_childs) >= 5: # with else (how about more if elses? maybe goes back to here)
-                        res = set.union(res, flows(if_childs[4]['children'], unsafe_variables, seen_methods, new_implicit_vars))
+                        res = set.union(res, flows(if_childs[4]['children'], seen_methods, new_implicit_vars))
                     # children will always exists
             case 'while_statement':
                 while_childs = node['children']
                 if len(while_childs) > 2: # not an empty while loop
                     guard_expression = while_childs[1] # skipping the "while" node
                     new_implicit_vars = set.union(find_ident_in_children(guard_expression['children']), implicit_variables)
-                    res = set.union(res, flows(while_childs[2]['children'], unsafe_variables, seen_methods, new_implicit_vars))
+                    res = set.union(res, flows(while_childs[2]['children'], seen_methods, new_implicit_vars))
                 #logger.debug(node['text'])
             case 'for_statement':
                 for_childs = node['children']
                 if len(for_childs) > 4: # Thank you for being a normal human being (for loop with initialization, termination, increment and body)
                     # for_childs[0] is just a node with text "for"
-                    init_flows = flows(for_childs[1]['children'], unsafe_variables, implicit_variables) # checking for flows in the initialization expression
+                    init_flows = flows(for_childs[1]['children'], implicit_variables) # checking for flows in the initialization expression
                     body_implicit_variables = set.union(find_ident_in_children(for_childs[2]['children']), implicit_variables) # checking for new implicit flow variables in termination expression
-                    update_expr_flows = flows(for_childs[3]['children'], unsafe_variables, seen_methods, body_implicit_variables)
-                    body_flows = flows(for_childs[4]['children'], unsafe_variables, seen_methods, body_implicit_variables) #two (or more) level flow from other expressions not added
+                    update_expr_flows = flows(for_childs[3]['children'], seen_methods, body_implicit_variables)
+                    body_flows = flows(for_childs[4]['children'], seen_methods, body_implicit_variables) #two (or more) level flow from other expressions not added
                     res = set.union(res, init_flows, update_expr_flows, body_flows)
                 elif len(for_childs) <= 4: # Who the fuck does this?
                     exit(f"For loop with {len(for_childs)} arguments")
             case 'expression_statement': #add update expression
                 #logger.debug(node)
-                res = set.union(res, flows(node['children'], unsafe_variables, seen_methods, implicit_variables))
+                res = set.union(res, flows(node['children'], seen_methods, implicit_variables))
             case 'method_invocation':
                 method_name = node['children'][0]['text']
                 if not method_name in seen_methods:
@@ -90,9 +96,9 @@ def flows(ast, unsafe_variables, seen_methods, implicit_variables=set()):
             case 'update_expression': # i++
                 updated_ident = node['children'][0]["text"]
                 total_idents = set.union(implicit_variables, updated_ident)
-                for var in unsafe_variables:
-                    if var in total_idents:
-                        res.add((var, updated_ident))
+                #for var in unsafe_variables:
+                for var in total_idents:
+                    res.add((var, updated_ident))
             #case 'block':
                 #logger.debug(node)
             case 'assignment_expression':
@@ -107,9 +113,8 @@ def flows(ast, unsafe_variables, seen_methods, implicit_variables=set()):
                 idents_from = find_ident_in_children(node['children'][1:]) # the identifier of variables using in expression
                 total_idents = set.union(idents_from, implicit_variables)
                 total_idents = set.union(total_idents, extra_idents)
-                for var in unsafe_variables:
-                    if var in total_idents:
-                        res.add((var, ident_to))
+                for var in total_idents:
+                    res.add((var, ident_to))
             case 'enhanced_for_statement':
                 for_each_childs = node['children']
                 #first child is for
@@ -119,19 +124,41 @@ def flows(ast, unsafe_variables, seen_methods, implicit_variables=set()):
                 #5th child is method_invocation
                 new_implicit_vars = set.union(implicit_variables, find_ident_in_children(for_each_childs[4]['children']))
                 #method_name = for_each_childs[4]['children'][0]['text']
-                method_flows = flows([for_each_childs[4]], unsafe_variables, seen_methods)
+                method_flows = flows([for_each_childs[4]], seen_methods)
                 #logger.debug(method_flows)
                 # if 
                 # new_seen_methods = seen_methods.append(method_name)
                 # method_flows = method_flow(method_name, )
                 #6th child is the body
-                body_flows = flows(for_each_childs[5]['children'], unsafe_variables, seen_methods, new_implicit_vars)
+                body_flows = flows(for_each_childs[5]['children'], seen_methods, new_implicit_vars)
                 res = set.union(res, body_flows)
                 res = set.union(res, method_flows)
                 #res = set.union(res, method_flows)
-
-
-                continue
+            case 'local_variable_declaration':
+                #logger.debug(node) # consists of type and variable declaration
+                res = set.union(res, flows(node['children'][1:], implicit_variables)) #variable declarator child. 1st child is type
+                #{'type': 'local_variable_declaration', 'text': 'String u = t + s;', 'children':
+                #  [
+                #   {'type': 'type_identifier', 'text': 'String', 'children': []}, 
+                #   {'type': 'variable_declarator', 'text': 'u = t + s', 'children': 
+                #       [{'type': 'identifier', 'text': 'u', 'children': []}, 
+                #       {'type': '=', 'text': '=', 'children': []}, 
+                #       {'type': 'binary_expression', 'text': 't + s', 'children': 
+                #           [{'type': 'identifier', 'text': 't', 'children': []}, 
+                #           {'type': '+', 'text': '+', 'children': []}, 
+                #           {'type': 'identifier', 'text': 's', 'children': []}]
+                #       }]
+                #   }]
+                #}
+            case 'variable_declarator':
+                var_dec_childs = node['children']
+                ident = var_dec_childs[0]['text']
+                #2nd child is =                
+                # expression used to declare
+                rhs_idents = find_ident_in_children(var_dec_childs[2:])
+                total_idents = set.union(rhs_idents, implicit_variables)
+                for var in total_idents:
+                    res.add((var, ident))
             case _:
                 continue
             # local variable declaration
@@ -210,13 +237,14 @@ def method_flow(method_name, seen_methods): #only works for method in the same c
     #         print(stmt.type, stmt.text.decode())
 
     ast_body = to_ast(body)
-    #logger.debug(ast_body['children'])
+    logger.debug(ast_body['children'])
     # for node in ast_body['children']:
     #     logger.debug(node)
     # logger.debug(f'method called: {method_name}')
     # logger.debug("-------------------\n")
     # logger.debug(ast_body)
-    flow_dict = flows(ast_body['children'], param_names, seen_methods)
+    #param_names.append("u")
+    flow_dict = flows(ast_body['children'], seen_methods)
     #logger.debug(flow_dict)
     return flow_dict
 
